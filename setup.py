e996fa8ba8bd65b96e3b7f0bb96b758f84b19486
Simon Willison <simon@simonwillison.net>
2009-12-26 12:17:41 +0000

    setup.py and pushed a django_cropper package to pypi

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import os
+
+setup(
+    name = 'django_cropper',
+    packages = ['cropper'],
+    version = '0.1.1a',
+    description = 'Image cropping for the Django admin',
+    author = 'Simon Willison',
+    author_email = 'simon@simonwillison.net',
+    url = 'http://github.com/simonw/django_cropper',
+    keywords = ['pil', 'django', 'cropping', 'image'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    long_description = open(
+        os.path.join(os.path.dirname(__file__), 'README.txt')
+    ).read().strip(),
+)
